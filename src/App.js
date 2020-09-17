3df43a0e973f1d37e364255fad57d42b7dedd025
Anthony COLAS <anthonycolas.france@gmail.com>
2020-09-17 17:51:51 +0200

    Hot temperature card OK

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
-import WeatherCard from 'components/WeatherCard/component';
-import './App.css';
-
+import WeatherCard from "components/WeatherCard/component";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <WeatherCard/>
+      <WeatherCard temp="28" />
+      <WeatherCard temp="15" />
+      <WeatherCard temp="40" />
     </div>
   );
-
 }
 
 export default App;
--- src/components/WeatherCard/component.js
+++ src/components/WeatherCard/component.js
@@ -1,31 +1,38 @@
-import React from 'react';
-import styled from '@emotion/styled';
+import React from "react";
+import styled from "@emotion/styled";
 
-import Location from "components/WeatherCard/Location"
-import Condition from "components/WeatherCard/Condition"
-import Icon from "components/WeatherCard/Icon"
+import Location from "components/WeatherCard/Location";
+import Condition from "components/WeatherCard/Condition";
+import Icon from "components/WeatherCard/Icon";
 
-const WeatherCard = (props) => {
+const WeatherCard = props => {
+  let highColor = (1 - (props.temp - 12) / 28) * 255;
+  let lowColor = highColor - 150;
+  console.log(lowColor);
+  console.log(highColor);
+  const Card = styled.div`
+    margin: 0 auto;
+    background: linear-gradient(
+      to top,
+      rgb(255, ${highColor}, 0),
+      rgb(255, ${lowColor}, 0)
+    );
+    width: 200px;
+    height: 240px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    border-radius: 15px;
+  `;
 
-	const Card = styled.div`
-		margin: 0 auto;
-		background: linear-gradient(to bottom, blue, lightblue);
-		width: 200px;
-		height: 240px;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		align-items: center;
-		border-radius: 15px;
-	`
-
-	return ( 
-			<Card>
-				<Location />
-				<Icon />
-				<Condition />
-			</Card>
-	);
-}
+  return (
+    <Card>
+      <Location />
+      <Icon />
+      <Condition />
+    </Card>
+  );
+};
 
 export default WeatherCard;
