15327bf0a8563e8448c348446214f538591907aa
Anthony COLAS <anthonycolas.france@gmail.com>
2020-09-17 18:58:18 +0200

    Cold weather card OK + weather icons

--- src/components/WeatherCard/component.js
+++ src/components/WeatherCard/component.js
@@ -6,17 +6,33 @@ import Condition from "components/WeatherCard/Condition";
 import Icon from "components/WeatherCard/Icon";
 
 const WeatherCard = props => {
-  let highColor = (1 - (props.temp - 12) / 28) * 255;
-  let lowColor = highColor - 150;
+  let highColor = 0;
+  let lowColor = 0;
+  let bg = null;
+  if (props.temp > 12) {
+    // Hot weather mechanics
+    highColor = (1 - (props.temp - 12) / 28) * 255;
+    lowColor = highColor - 150;
+    bg = `linear-gradient(
+      to top,
+      rgb(255, ${highColor}, 0),
+      rgb(255, ${lowColor}, 0)
+    )`;
+  } else if (props.temp <= 12) {
+    // Cold weather mechanics
+    highColor = (1 - (props.temp + 20) / 32) * 255;
+    lowColor = highColor - 150;
+    bg = `linear-gradient(
+      to top,
+      rgb(0, ${highColor}, 255),
+      rgb(0, ${lowColor}, 255)
+    )`;
+  }
   console.log(lowColor);
   console.log(highColor);
   const Card = styled.div`
     margin: 0 auto;
-    background: linear-gradient(
-      to top,
-      rgb(255, ${highColor}, 0),
-      rgb(255, ${lowColor}, 0)
-    );
+    background: ${bg};
     width: 200px;
     height: 240px;
     display: flex;
